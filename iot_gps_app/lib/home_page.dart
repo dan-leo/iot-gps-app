ab0798ad3c00cf1467f224672c36440357e8c532
Daniel Robinson <daniel@flycloudline.com>
2020-06-12 03:02:24 +0200

    periodic lat-long updates to thingsboard.io backend

--- iot_gps_app/lib/home_page.dart
+++ iot_gps_app/lib/home_page.dart
@@ -1,39 +1,11 @@
 import 'dart:io';
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:geolocator/geolocator.dart';
 import 'package:neat_periodic_task/neat_periodic_task.dart';
 // import 'package:flutter/scheduler.dart';
 // import 'package:flutter/services.dart';
-
-Position _currentPosition;
-int timeElapsed = 0;
-
-void postGPS() {
-  if (_currentPosition != null)
-    print("$timeElapsed | LAT: ${_currentPosition.latitude}, LNG: ${_currentPosition.longitude}");
-  else
-    print("$timeElapsed | Waiting for GPS lock ...");
-  // setState(() {
-  timeElapsed++;
-  // });
-}
-
-
-Future<void> scheduleTask() async {
-  // Create a periodic task that prints 'Hello World' every 1s
-  final scheduler = NeatPeriodicTaskScheduler(
-    interval: Duration(milliseconds: 1000),
-    name: 'stream-gps',
-    timeout: Duration(milliseconds: 500),
-    task: () async => postGPS(),
-    minCycle: Duration(milliseconds: 500),
-  );
-
-  scheduler.start();
-  await ProcessSignal.sigterm.watch().first;
-  await scheduler.stop();
-}
+// import 'dart:async';
 
 class HomePage extends StatefulWidget {
   @override
@@ -43,13 +15,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
+  Position _currentPosition;
   String _currentAddress;
   bool runOnce = true;
+  int timeElapsed = 0;
 
   @override
   Widget build(BuildContext context) {
     if (runOnce) {
-      scheduleTask();
+      _scheduleTask();
       runOnce = false;
     }
     return Scaffold(
@@ -60,6 +34,7 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text("Elapsed time: $timeElapsed"),
             (_currentPosition != null) 
               ? Text("LAT: ${_currentPosition.latitude}, LNG: ${_currentPosition.longitude}")
               : Text("Waiting for GPS lock ..."),
@@ -77,6 +52,26 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  _oneSecond() {
+    _getCurrentLocation();
+    timeElapsed++;
+  }
+
+  _scheduleTask() async {
+    // Create a periodic task that prints 'Hello World' every 1s
+    final scheduler = NeatPeriodicTaskScheduler(
+      interval: Duration(milliseconds: 1000),
+      name: 'stream-gps',
+      timeout: Duration(milliseconds: 999),
+      task: () async => _oneSecond(),
+      minCycle: Duration(milliseconds: 500),
+    );
+
+    scheduler.start();
+    await ProcessSignal.sigterm.watch().first;
+    await scheduler.stop();
+  }
+
   _getCurrentLocation() {
     final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
@@ -91,6 +86,8 @@ class _HomePageState extends State<HomePage> {
     }).catchError((e) {
       print(e);
     });
+
+    _postGPS();
   }
 
   _getAddressFromLatLng() async {
@@ -108,4 +105,43 @@ class _HomePageState extends State<HomePage> {
       print(e);
     }
   }
+
+  _postGPS() {
+    try {
+      if (_currentPosition != null) {
+        print("$timeElapsed | LAT: ${_currentPosition.latitude}, LNG: ${_currentPosition.longitude}");
+        postRequest(_currentPosition.latitude.toString(), _currentPosition.longitude.toString());
+      }
+      else
+        print("$timeElapsed | Waiting for GPS lock ...");
+    } catch (e) {
+      print(e);
+    }
+  }
+}
+
+postRequest(String lat, String long) async {
+  final accessToken = 'FBprmkoajCHYwRNmSsSv';
+  var url = 'https://demo.thingsboard.io/api/v1/$accessToken/telemetry';
+  // url = 'https://ptsv2.com/t/pm7ab-1591921811/post';
+  var body = '{"lat": $lat, "long": $long}';
+
+  print('Url: $url, Body: $body');
+
+  var response = await http.post(
+    url,
+    headers: {
+      'Accept': '*/*',
+      'Content-Type': 'application/json',
+    },
+    body: body,
+    encoding: null
+  );
+
+  if (response.body.isNotEmpty)
+    print("response.body: ${response.body}");
+
+  // todo - handle non-200 status code, etc
+
+  return response.body;
 }
